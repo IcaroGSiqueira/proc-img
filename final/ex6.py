709d9a0f1ae4bbafbee5f85d74b4996b5967f982
icaroGS <icaro@bemobile.tech>
2022-07-05 20:39:00 -0300

    exame ex6 borda 4 e 8

--- final/ex6.py
+++ final/ex6.py
@@ -3,13 +3,35 @@ import numpy as np
 
 img = cv2.imread('./orig_images/folha.png', 0)
 
-new_img = img.copy()
+img_borda_adj4 = img.copy()
+img_borda_adj8 = img.copy()
 
 for i in range(1, len(img)-1):
     for j in range(1, len(img[0])-1):
-        if (not(img[i+1][j+1] == img[i-1][j+1] == img[i+1][j-1] == img[i-1][j-1])):
-            new_img[i][j] = 0
+        if (not(
+            img[i+1][j+1] ==
+            img[i-1][j+1] ==
+            img[i+1][j-1] ==
+            img[i-1][j-1]
+            )):
+            img_borda_adj4[i][j] = 0
         else:
-            new_img[i][j] = 255
+            img_borda_adj4[i][j] = 255
 
-cv2.imwrite('ex6/folha_borda.jpg', new_img)
+        if (not(
+            img[i+1][j+1] ==
+            img[i-1][j+1] ==
+            img[i][j+1] ==
+            img[i-1][j-1] ==
+            img[i+1][j-1] ==
+            img[i][j-1] ==
+            img[i+1][j] ==
+            img[i-1][j]
+            )):
+            img_borda_adj8[i][j] = 0
+        else:
+            img_borda_adj8[i][j] = 255
+
+cv2.imwrite('ex6/folha_borda_adj4.jpg', img_borda_adj4)
+
+cv2.imwrite('ex6/folha_borda_adj8.jpg', img_borda_adj8)
